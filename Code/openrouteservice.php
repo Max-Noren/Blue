37b8ef334fddbba856b9ddfa3f5303de00dfdb4a
Max Norén <70686800+Max-Noren@users.noreply.github.com>
2021-09-24 12:15:29 +0200

    Update openrouteservice.php
    Functioning autocomplete function. Now only requires correct output

--- Code/openrouteservice.php
+++ Code/openrouteservice.php
@@ -15,54 +15,51 @@
 
     </head>
     <body>
+        
+        <!-- Temporary -->
+        <p id='demo'></p>
 
         <script>
-
-          //Place to put all addresses!
-          function addressFunc(){
-              
-              var addresses = [
-                "Chalmers",
-                "Mölndal",
-                "Centralstation",
-                "Haga",
-                "Majorna",
-                "Sävedalen",
-                "Kållered",
-                "Kåltorp",
-                "Linné",
-                "Järntorget",
-                "Saltholmen",
-                "Askim"
-              ];
-             
-            return addresses;
-          }
+            
+            //Global array for available addresses
+            var allAddresses = ["Missing"];
           function myPrint(){
               document.getElementById("demo").innerHTML = Math.random();
           }
 
             //Autocomplete for start address
           $( function() {
+              $("#start").on("input", function(){
+                  getAddress($(this).val());
+              })
+              
             $( "#start" ).autocomplete({
-              source: addressFunc()
+              source: allAddresses
             });  
           } );
 
               //Autocomplete for end address
           $( function() {
+                $("#end").on("input", function(){
+                  getAddress($(this).val());
+              })
             $( "#end" ).autocomplete({
-              source: addressFunc()
+              source: allAddresses
             });
           } );
             
             
+          
             
-            function getAddress(){
+                            
+            function getAddress(search){
+                
                 var request = new XMLHttpRequest();
 
-                request.open('GET', 'https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf624886866da0e6fb41b5a3cb1b1f8f9954d7&text=Gothenburg&boundary.country=SE&sources=openstreetmap,openaddresses');
+                //request.open('GET', 'https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf624886866da0e6fb41b5a3cb1b1f8f9954d7&text='+search+'&boundary.country=SE&sources=openstreetmap,openaddresses');
 
+                request.open('GET', 'https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248e38d3f64310c46ceb044c9b37ef563cd&text='+search+'&boundary.country=SE&sources=openstreetmap&layers=neighbourhood,address,venue');
+                
                 request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
 
                 request.onreadystatechange = function () {
@@ -71,23 +68,36 @@
                     console.log('Headers:', this.getAllResponseHeaders());
                     console.log('Body:', this.responseText);
                       
+                      
+                    var response = JSON.parse(this.responseText);
+                
+                    //document.getElementById('demo').innerHTML = response.features[0].properties.name;
+                      
+                    var properties = response;
+                    //document.getElementById('demo').innerHTML = count(properties);
+                      
+                    for (var i = 0; i < 10; i++){
+                        var address = response.features[i].properties.label;
+                        document.getElementById('demo').innerHTML = address;
+                        allAddresses[i] = address;
+                        
+                    }                      
+                      
                   }
                 };
 
                 //This crashes the shit
                 request.send();
-                document.write("Trying");
                 
-                var response = JSON.stringify(request);
-                
-                var features = response.features;
-                var name = response.name;
-                document.write(name);
-                
-                
-                document.write("Done!");
             }
-            getAddress();
+            
+              $("#start").on("input",function (){
+                var search = $(this).val();
+                document.write(search);
+                getAddress(search);
+                
+            })
+            getAddress("Majorna");
           </script>
 
         
