87d06a63eda08be8cdc3440ef55a079b7db9ef35
jopers <joel.persson.p@gmail.com>
2021-09-24 13:55:39 +0200

    PHPUnit test enviroment added

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
-<HTML>
-    <head>
-        <title>Blue</title>
-    </head>
-    <body style="background-color:blue;">
+<?php
 
-        <?php  
-            echo '<strong><p style="color: powderblue; font-family: Arial; font-size: 60px; text-align: center; margin: auto; width: 50%;">Blue<p></strong>';
-        ?>  
-    
+#____________________
+#Upon press of the 'Calculate' button it runs function input_co2()
+#____________________
+
+if($_GET['btn_submit'])
+{
+    input_co2($_GET['car_emission'], $_GET['car_distance']);
+    calculate_co2($_GET['car_emission'], $_GET['car_distance']);
+}
+
+
+#____________________
+#Function that takes a variable and prints it
+#____________________
+
+ function input_co2($emission, $distance)#$quantity);
+ {
+     echo "This is your emission --> " . $emission . '<br>';
+     echo "This is your route distance --> " . $distance . '<br><br>';
+ 
+ }
+
+#____________________
+# This function calculated the total emission for the route, using
+# inputs for emission & distance, then prints it out. 
+#____________________
+ function calculate_co2($emission, $distance)
+ {
+    $tot_emission = $emission * $distance;
+    echo "The total emission for your route is: " . $tot_emission . "g CO2" . '<br><br><br>';
+    return $tot_emission;
+ }
+
+
+?>
+
+<html>
     <body>
 </HTML>
---
+++ tests/FunctionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'C:\Users\Joel\Desktop\GIT\Blue\index.php';
+
+class FunctionTest extends \PHPUnit\Framework\TestCase {
+    public function testTotalEmission(){
+        $result = calculate_co2(2,2);
+
+        $this->assertEquals(4, $result);
+    }
+}
