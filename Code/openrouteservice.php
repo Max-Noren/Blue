e9ed958999a84f7fd3bcea323041f544ed98da6e
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2021-09-28 18:50:18 +0200

    php section deleted and code simplfied

--- Code/openrouteservice.php
+++ Code/openrouteservice.php
@@ -30,13 +30,13 @@
       <script>
 
         //Global array for available addresses
-        var allAddresses = ["N/A"];
+        const allAddresses = ["N/A"];
 
         //Autocomplete for start address
         $( function() {
             
           $("#start").on("input", function(){ //Calls on the getAddress function every keystroke
-            getAddress($(this).val());
+            getAddress($(this).val(), "start");
           })
 
           $( "#start" ).autocomplete({
@@ -49,7 +49,7 @@
         $( function() {
           
           $("#end").on("input", function(){ //Calls on the getAddress function every keystroke
-            getAddress($(this).val());
+            getAddress($(this).val(), "end");
           })
           
           $( "#end" ).autocomplete({
@@ -59,8 +59,7 @@
         } );
 
         //Obtains related addresses based on search, Openrouteservice API
-        function getAddress(search){
-
+        function getAddress(search, startOrEnd){
             var request = new XMLHttpRequest();
 
             request.open('GET', 'https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248e38d3f64310c46ceb044c9b37ef563cd&text='+search+'&boundary.country=SE&sources=openstreetmap&layers=neighbourhood,address,venue');
@@ -69,19 +68,24 @@
 
             request.onreadystatechange = function () {
               if (this.readyState === 4) {
-                console.log('Status:', this.status);
-                console.log('Headers:', this.getAllResponseHeaders());
-                console.log('Body:', this.responseText);
+                //console.log('Status:', this.status);
+                //console.log('Headers:', this.getAllResponseHeaders());
+                //console.log('Body:', this.responseText);
 
 
                 var response = JSON.parse(this.responseText);
+                while (allAddresses.length) { allAddresses.pop(); } // Empty array
+                response.features.map(feature => feature.properties.label).forEach(label => allAddresses.push(label));  // Filter our label from response and push to allAddresses
 
-                for (var i = 0; i < 10; i++){
-                    var address = response.features[i].properties.label;
-                    allAddresses[i] = address;
-
-                }                      
-
+                if(startOrEnd == "start") {
+                  $("#startLat").val(response.features[0].geometry.coordinates[1]);
+                  $("#startLng").val(response.features[0].geometry.coordinates[0]);
+                } else if(startOrEnd == "end") {
+                  $("#endLat").val(response.features[0].geometry.coordinates[1]);
+                  $("#endLng").val(response.features[0].geometry.coordinates[0]);
+                } else {
+                  console.warn("Invalid startOrEnd arg", startOrEnd);
+                }
               }
             };
 
@@ -97,14 +101,19 @@
         
           <label for="start">Start Address: </label>
           <input id="start" name="start" >
-            
+
           <br>
-            
+
           <label for="end">End Address: </label>
           <input id="end" name="end" >
             
           <br>
-            
+          <input style="display: none;" id="startLat" name="startLat" >
+          <input style="display: none;" id="startLng" name="startLng" >
+          <input style="display: none;" id="endLat" name="endLat" >
+          <input style="display: none;" id="endLng" name="endLng" >
+
+
           <input type="submit" name="btn_submit" value="My Carbon Footprint" />
             
         </form> 
@@ -113,61 +122,23 @@
       <?php
         // Displayes coordinates from the search
         if(isset($_GET['btn_submit']))
-        {  
-	
-			$start = $_GET['start'];
-			$end = $_GET['end'];
-			
-			$start_coordinates = getAddresses($start);
-			$end_coordinates = getAddresses($end);
-			
-			
-			echo "<br>Start : ", $start, "<br>";
-			echo "Latitude : ", $start_coordinates[1], "<br>";
-			echo "Longitude : ", $start_coordinates[0], "<br>";
-			
-			echo "<br>End : ", $end, "<br>";
-			echo "Latitude : ", $end_coordinates[1], "<br>";
-			echo "Longitude : ", $end_coordinates[0], "<br>";
-			
-        }
-        
-        //Obtains related addresses based on search, Openrouteservice API. Returns coordinates
-        function getAddresses($search){
-            $searchUrlEncode = "";
+        {
 
-          for ($i = 0; $i <= strlen($search); $i++) {
-            if(substr($search,strlen($search)-strlen($search)+$i,1)==" "){
-              $temp = "%20";
-            }
-            else{
-              $temp = substr($search,strlen($search)-strlen($search)+$i,1);
-            }
-            $searchUrlEncode = $searchUrlEncode.$temp;
-          };
-          //echo '<script>alert("$searchUrlEncode")</script>';
-          $ch = curl_init();
-
-          curl_setopt($ch, CURLOPT_URL, "https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf624886866da0e6fb41b5a3cb1b1f8f9954d7&text=".$searchUrlEncode."&boundary.country=SE&sources=openstreetmap,openaddresses&layers=neighbourhood,address,venue");
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-          curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Accept: application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8"
-          ));
-          $response = curl_exec($ch);
-          curl_close($ch);
+			    $start = $_GET['start'];
+          $end = $_GET['end'];
+          $startLat = $_GET['startLat'];
+          $endLat = $_GET['endLat'];
+			    $startLng = $_GET['startLng'];
+          $endLng = $_GET['endLng'];
 			
-          $features = json_decode($response)->features;
-        
-          if(empty($features)){
-            return ["N/A","N/A"];
-          }
-          $coordinates = json_decode($response)->features[0]->geometry->coordinates;
-          
-          return $coordinates;
+            echo "<br>Start : ", $start, "<br>";
+            echo "Latitude : ", $startLat, "<br>";
+            echo "Longitude : ", $startLng, "<br>";
+
+            echo "<br>End : ", $end, "<br>";
+            echo "Latitude : ", $endLat, "<br>";
+            echo "Longitude : ", $endLng, "<br>";
         }
       ?>
-
     </body>
 </html>
