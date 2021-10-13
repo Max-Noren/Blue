8400ecf415b6dbe3755b69982311e6c530e05f9f
Max-Noren <70686800+Max-Noren@users.noreply.github.com>
2021-10-13 14:40:48 +0200

    Update logo and title
    Co-Authored-By: lisaloving <78026882+lisaloving@users.noreply.github.com>

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Carbon Mort</title>
+        <title>Blue</title>
         
         <!-- jQuery Widgets -->
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
