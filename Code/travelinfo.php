c6c3db9de52b5ce0a666d459ee4c7d05555996a8
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2021-10-16 12:51:38 +0200

    URLencode added again

--- Code/travelinfo.php
+++ Code/travelinfo.php
@@ -42,7 +42,7 @@
   function getCoordinatesORSAutoComplete($placeName){
     $curl_ORS = curl_init();
 
-    curl_setopt($curl_ORS, CURLOPT_URL, "https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248e38d3f64310c46ceb044c9b37ef563cd&text=".$placeName."&boundary.country=SE&sources=openstreetmap&layers=neighbourhood,address,venue&boundary.circle.lon=11.97307942&boundary.circle.lat=57.70914870&boundary.circle.radius=11");
+    curl_setopt($curl_ORS, CURLOPT_URL, "https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248e38d3f64310c46ceb044c9b37ef563cd&text=".urlencode($placeName)."&boundary.country=SE&sources=openstreetmap&layers=neighbourhood,address,venue&boundary.circle.lon=11.97307942&boundary.circle.lat=57.70914870&boundary.circle.radius=11");
     curl_setopt($curl_ORS, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($curl_ORS, CURLOPT_HEADER, FALSE);
 
